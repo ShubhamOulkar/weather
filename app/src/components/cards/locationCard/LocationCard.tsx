14ea648646f22a49d4e1af8b4911db4711e558a4
Shubham Oulkar <oulkarshubhu@gmail.com>
2025-09-22 17:46:24 +0530

    add location card

--- app/src/components/cards/locationCard/LocationCard.tsx
+++ app/src/components/cards/locationCard/LocationCard.tsx
@@ -1,7 +1,7 @@
 import LocBackSmall from "../../../assets/images/bg-today-small.svg"
 import LocBackLarge from "../../../assets/images/bg-today-large.svg"
 import IconRaining from "../../../assets/images/icon-rain.webp"
-import cnr from "../../../utils/cnr"
+import cnr from "../../../utils/class_resolver/cnr"
 import styles from "./LocationCard.module.css"
 
 export default function LocationCard() {
@@ -12,12 +12,12 @@ export default function LocationCard() {
         </picture>
         <div className={cnr('flexcol', 'flexcenter', styles.location_details)}>
             <div className="flexcol gap-1rem">
-                <h2>Hupari, India</h2>
+                <h2 className="txt-no-wrap">Hupari, India</h2>
                 <sub>Sunday, Sept 14, 2025</sub>
             </div>
             <div className="flex">
                 <img className={styles.location_weather_icon} src={IconRaining} alt="raining" />
-                <p className={styles.location_temp} >20<sup> o</sup></p>
+                <p className={styles.location_temp} >20°C</p>
             </div>
         </div>
     </div>
