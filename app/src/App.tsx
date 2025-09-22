b22f6a4d90077173865f25f81d56e69c0e888881
Shubham Oulkar <oulkarshubhu@gmail.com>
2025-09-22 17:45:01 +0530

    add main grid layout

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,7 @@
-import { Toolbar } from './components/toolbar/Toolbar'
-import Title from './components/titleWithSearch/Title'
-import SearchForm from './components/titleWithSearch/Search'
-import { Dashboard } from './components/dashboard/Dashboard'
+import Toolbar from './components/toolbar/Toolbar'
+import Title from './components/title/Title'
+import SearchForm from './components/search/Search'
+import Dashboard from './components/dashboard/Dashboard'
 import styles from './App.module.css'
 
 function App() {
