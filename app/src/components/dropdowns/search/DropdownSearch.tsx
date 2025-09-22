e16ed77013af67d83712aa246dc6ee905758a7b7
Shubham Oulkar <oulkarshubhu@gmail.com>
2025-09-22 17:48:36 +0530

    add search component

--- app/src/components/dropdowns/search/DropdownSearch.tsx
+++ app/src/components/dropdowns/search/DropdownSearch.tsx
@@ -1,15 +1,26 @@
-import cnr from "../../../utils/cnr"
+import type { Ref } from "react";
+import cnr from "../../../utils/class_resolver/cnr"
+import DropBtn from "../../common/dropButton/DropBtn";
 import styles from "./DropdownSearch.module.css"
 
 interface CompArg {
-    dropdown: boolean
+    id: string;
+    dismissRef: Ref<HTMLDivElement>;
+    dropdown: boolean;
 }
 
-export default function DropdownSearch({ dropdown }: CompArg) {
-    return <div className={cnr('scroll', styles.drop_places, dropdown ? 'show' : 'hidden')}>
+export default function DropdownSearch({ id, dismissRef, dropdown }: CompArg) {
+    // TODO: close the drop down if place is selected by user, outside dismissal stops closing 
+    return <div id={id}
+        ref={dismissRef}
+        role="listbox"
+        aria-label="list of places"
+        aria-hidden={!dropdown}
+        aria-live="polite"
+        className={cnr('scroll', styles.drop_places, dropdown ? 'show' : 'hidden')}>
         <ul role="list">
-            <li>City name</li>
-            <li>City name</li>
+            <li><DropBtn btnTitle="City name"/></li>
+            <li><DropBtn btnTitle="City name"/></li>
         </ul>
     </div>
 }
