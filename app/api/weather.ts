993fa38e516dfd74a734ce2b2b0fd0b9371ddeb5
Shubham Oulkar <oulkarshubhu@gmail.com>
2025-10-13 08:10:16 +0530

    correct query params

--- app/api/weather.ts
+++ app/api/weather.ts
@@ -3,8 +3,8 @@ import { generateImage } from "../src/utils/ogImage/generateOgImage";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     try {
-        const { place = "Hupari", temp = "25", wmo = "61" } = req.query;
-        const data = { city: place, temperature: temp, wmo: Number(wmo) };
+        const { name = "Hupari", temp = "25", wmo = "61" } = req.query;
+        const data = { city: name, temperature: temp, wmo: Number(wmo) };
         const { webp } = await generateImage(data);
         res.setHeader("Content-Type", "image/webp");
         res.setHeader("Content-Length", webp.length);
