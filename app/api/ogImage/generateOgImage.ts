e0434bdbe47efc28e114654b4b66f047b8f13f38
Shubham Oulkar <oulkarshubhu@gmail.com>
2025-10-13 10:02:58 +0530

    test

--- app/api/ogImage/generateOgImage.ts
+++ app/api/ogImage/generateOgImage.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import satori from "satori";
 import sharp from "sharp";
-import { getWeatherIcon } from "../../src/utils/getWeatherIcon/getWeatherIcon";
+import { getWeatherIcon } from "@/utils/getWeatherIcon/getWeatherIcon";
 
 // Define weather data type
 interface WeatherData {
