5efef4f319797e34f27de4bca39c2ee92a5ac75d
Shubham Oulkar <oulkarshubhu@gmail.com>
2025-10-16 12:14:02 +0530

    test

--- app/api/ogImage/generateOgImage.ts
+++ app/api/ogImage/generateOgImage.ts
@@ -33,9 +33,7 @@ export async function generateImage(data: WeatherData) {
     path.join(__dirname, "../../public/bg-today-small.svg"),
   );
 
-  const backgroundBuffer = (await sharp(backGround).png().toBuffer()).toString(
-    "base64",
-  );
+  const backgroundBuffer = Buffer.from(backGround).toString("base64");
 
   const iconBuffer = (await sharp(iconPath).png().toBuffer()).toString(
     "base64",
@@ -51,7 +49,7 @@ export async function generateImage(data: WeatherData) {
         gap: "32px",
         width: "100%",
         height: "100%",
-        backgroundImage: `data:image/png;base64,${backgroundBuffer}`,
+        backgroundImage: `data:image/svg+xml;base64,${backgroundBuffer}`,
         backgroundSize: "cover",
         backgroundPosition: "center",
         borderRadius: "8px",
