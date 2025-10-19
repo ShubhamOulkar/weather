780d8bf2ec110870be357981944e28735b89e121
Shubham Oulkar <oulkarshubhu@gmail.com>
2025-10-19 19:45:45 +0530

    doc: testing choice

--- app/src/components/common/copyBtn/__test__/CopyBtn.test.tsx
+++ app/src/components/common/copyBtn/__test__/CopyBtn.test.tsx
@@ -3,15 +3,20 @@ import "@testing-library/jest-dom";
 import type { ReactNode } from "react";
 import { beforeEach, describe, expect, it, vi } from "vitest";
 import AllProvider from "@/provider/AllProvider";
+import { getLocalDate } from "@/utils/local_date/getLocalDate";
 import CopyBtn from "../CopyBtn";
 
 const renderComponent = (children: ReactNode) => {
   return render(<AllProvider>{children}</AllProvider>);
 };
 
-describe("CopyBtn Component", () => {
+describe("CopyBtn Integration Test", () => {
   beforeEach(() => {
     vi.resetAllMocks();
+    Object.defineProperty(window, "location", {
+      value: { href: "https://example.com/" },
+      writable: true,
+    });
   });
 
   it("renders correctly in its initial state", () => {
@@ -32,8 +37,18 @@ describe("CopyBtn Component", () => {
     const button = screen.getByRole("button", { name: /copy url/i });
     fireEvent.click(button);
 
+    const fullDate = encodeURIComponent(getLocalDate().fullDate);
+    const time = encodeURIComponent(
+      getLocalDate(undefined, { minute: "2-digit" }).time,
+    );
+
     await waitFor(() => {
       expect(mockWriteText).toHaveBeenCalled();
+      const copiedText = mockWriteText.mock.calls[0][0];
+      // Expect full correct URL now
+      expect(copiedText).toMatch(
+        `https://example.com/api/weather-card?name=Hupari%2C%20MH%2C%20IN&temp=20&wmo=96&date=${fullDate}&time=${time}`,
+      );
       // toast notification
       expect(screen.getByText(/url copied!/i)).toBeInTheDocument();
     });
@@ -52,6 +67,9 @@ describe("CopyBtn Component", () => {
 
     await waitFor(() => {
       expect(mockWriteText).toHaveBeenCalled();
+      const copiedText = mockWriteText.mock.calls[0][0];
+      // empty copied url
+      expect(copiedText).toMatch("");
       // toast notification
       expect(screen.getByText(/failed to copy url/i)).toBeInTheDocument();
     });
